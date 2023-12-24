7286f83862014b1be1c1439663b9594c15846327
Rahmanism <s.mostafa.rahmani@gmail.com>
2023-12-24 13:09:40 +0330

    Add encryption/decryption using AES

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+string key = "sdfwertdfghrthdsfwegdfh5645y7ftr";
+
+Console.WriteLine("Please enter a string to encrypt");
+string plainText = Console.ReadLine();
+string cipherText = AesOperation.Encrypt(key, plainText);
+Console.WriteLine(cipherText);
+
+string decryptedText = AesOperation.Decrypt(key, cipherText);
+Console.WriteLine($"decrypted text: {decryptedText}");
+
+Console.ReadKey();
